1284ee625d7a483d770de2d13d54b2ae37242cde
MyOwnSling <myownsling@gmail.com>
2015-05-24 22:01:24 -0600

    Added print methods to weather classes
    Print methods include options for choice data points and all data points

--- getWeather.py
+++ getWeather.py
@@ -25,41 +25,50 @@ forecastInfo = getForecast(forecastAPIKey, forecastLocation)
 
 weather = Weather(forecastInfo)
 
-#min = Minutely(jsonRoot)
-print weather.minutely.summary
-print weather.minutely.minutes[0].precipProbability
-print ''
+weather.location.printData()
+print '************************************************************************************'
+# weather.daily.printData(True)
 
-#cur = Currently(jsonRoot)
-print weather.currently.time
-print weather.currently.temperature
-print ''
+# for min in weather.minutely.minutes:
+#     for data in min:
+#         print data, min[data]
+#     print
 
-#hour = Hourly(jsonRoot)
-print weather.hourly.summary
-print weather.hourly.hours[12].time
-print weather.hourly.hours[12].temperature
-# for hour in weather.hourly.hours:
-#     print hour.time, hour.temperature
-print weather.hourly.hours[12].summary
-print ''
+#weather.currently.printData(True)
 
-#day = Daily(jsonRoot)
-print weather.daily.summary
-print weather.daily.days[2].time
-print weather.daily.days[2].temperatureMax
-print weather.daily.days[2].summary
-print ''
-
-#loc = Location(jsonRoot)
-print weather.location.latitude, weather.location.longitude
-print weather.location.timezone
-print weather.location.offset
-print ''
-
-print "**************************"
-for minute in weather.minutely.minutes:
-    for metric in minute:
-        print metric, ":", minute[metric]
-
-print 'Done'
+# #min = Minutely(jsonRoot)
+# print weather.minutely.summary
+# print weather.minutely.minutes[0].precipProbability
+# print ''
+#
+# #cur = Currently(jsonRoot)
+# print weather.currently.time
+# print weather.currently.temperature
+# print ''
+#
+# #hour = Hourly(jsonRoot)
+# print weather.hourly.summary
+# print weather.hourly.hours[12].time
+# print weather.hourly.hours[12].temperature
+# # for hour in weather.hourly.hours:
+# #     print hour.time, hour.temperature
+# print weather.hourly.hours[12].summary
+# print ''
+#
+# #day = Daily(jsonRoot)
+# print weather.daily.summary
+# print weather.daily.days[2].time
+# print weather.daily.days[2].temperatureMax
+# print weather.daily.days[2].summary
+# print ''
+#
+# #loc = Location(jsonRoot)
+# print weather.location.latitude, weather.location.longitude
+# print weather.location.timezone
+# print weather.location.offset
+# print ''
+#
+# print "**************************"
+# for minute in weather.minutely.minutes:
+#     for metric in minute:
+#         print metric, ":", minute[metric]
--- weatherTypes.py
+++ weatherTypes.py
@@ -21,6 +21,7 @@ class _dataPoint(dict):
         self.precipIntensity            = dataBlock.get('precipIntensity')
         self.precipProbability          = dataBlock.get('precipProbability')
         self.precipType                 = dataBlock.get('precipType')
+        self.precipAccumulation         = dataBlock.get('precipAccumulation')
         self.temperature                = dataBlock.get('temperature')
         self.apparentTemperature        = dataBlock.get('apparentTemperature')
         self.dewPoint                   = dataBlock.get('dewPoint')
@@ -77,15 +78,43 @@ class Currently(_dataPoint):
         currently = jsonDict.get('currently')
         if not currently:
             print 'Bad json dictionary; no "currently" data'
-            return None
+            return
         _dataPoint.__init__(self, currently)
+
+    def printData(self, full=False):
+        if full:
+            print 'time', ':', self.time
+            print 'summary', ':', self.summary
+            print
+            for key in self:
+                if key != 'time' and key != 'summary':
+                    print key, ':', self[key]
+        else:
+            print self.time
+            print self.summary
+            print
+            print 'Temperature: ', self.temperature, 'F'
+            print 'Wind speed:', self.windSpeed, 'MPH'
+            print 'Wind bearing (from):', self.windBearing, 'deg'
+            print 'Precipitation chance:', self.precipProbability * 100, '%'
+            print
+            if self.nearestStormDistance != None:
+                print 'Nearest storm distance:', self.nearestStormDistance, 'mi'
+                print 'Nearest storm bearing:', self.nearestStormBearing, 'deg'
+                print
+            print 'Humidity:', self.humidity * 100, '%'
+            print 'Dew point:', self.dewPoint, 'F'
+            print 'Visibility:', self.visibility, 'mi'
+            print 'Cloud cover:', self.cloudCover * 100, '%'
+            print 'Pressure:', self.pressure, 'mb'
+            print 'Ozone:', self.ozone
 
 class Minutely():
     def __init__(self, jsonDict):
         minutely = jsonDict.get('minutely')
         if not minutely:
             print 'Bad json dictionary; no "minutely" data'
-            return None
+            return
         self.summary = minutely.get('summary')
         self.icon = minutely.get('icon')
         self.minutes = []
@@ -93,12 +122,31 @@ class Minutely():
         for minute in data:
             self.minutes.append(_dataPoint(minute))
 
+    def printData(self, full=False):
+        print
+        print self.summary
+        if full:
+            for minute in self.minutes:
+                for key in minute:
+                    print key, ':', minute[key]
+                print
+        else:
+            for minute in self.minutes:
+                print
+                print str(minute.time).split(' ')[1]
+                print '\tPrecipitation chance:', minute.precipProbability * 100, '%'
+                if minute.precipProbability != 0:
+                    print '\tType:', minute.precipType
+                    print '\tIntensity:', minute.precipIntensity, 'in/hr'
+                if minute.precipAccumulation != None:
+                    print '\tAccumulation:', minute.precipIntensity, 'in/hr'
+
 class Hourly():
     def __init__(self, jsonDict):
         hourly = jsonDict.get('hourly')
         if not hourly:
             print 'Bad json dictionary; no "hourly" data'
-            return None
+            return
         self.summary = hourly.get('summary')
         self.icon = hourly.get('icon')
         self.hours = []
@@ -106,12 +154,48 @@ class Hourly():
         for hour in data:
             self.hours.append(_dataPoint(hour))
 
+    def printData(self, full=False):
+        print
+        print self.summary
+        if full:
+            for hour in self.hours:
+                for key in hour:
+                    print key, ':', hour[key]
+                print
+        else:
+            for hour in self.hours:
+                print
+                print hour.time
+                print '\t', hour.summary
+                print
+                print '\tTemperature: ', hour.temperature, 'F'
+                print '\tWind speed:', hour.windSpeed, 'MPH'
+                print '\tWind bearing (from):', hour.windBearing, 'deg'
+                print
+                print '\tPrecipitation chance:', hour.precipProbability * 100, '%'
+                if hour.precipProbability != 0:
+                    print '\tType:', hour.precipType
+                    print '\tIntensity', hour.precipIntensity, 'in/hr'
+                if hour.precipAccumulation != None:
+                    print '\tAccumulation:', hour.precipAccumulation, 'in'
+                print
+                if hour.nearestStormDistance != None:
+                    print 'Nearest storm distance:', hour.nearestStormDistance, 'mi'
+                    print 'Nearest storm bearing:', hour.nearestStormBearing, 'deg'
+                    print
+                print '\tHumidity:', hour.humidity * 100, '%'
+                print '\tDew point:', hour.dewPoint, 'F'
+                print '\tVisibility:', hour.visibility, 'mi'
+                print '\tCloud cover:', hour.cloudCover * 100, '%'
+                print '\tPressure:', hour.pressure, 'mb'
+                print '\tOzone:', hour.ozone
+
 class Daily():
     def __init__(self, jsonDict):
         daily = jsonDict.get('daily')
         if not daily:
             print 'Bad json dictionary; no "daily" data'
-            return None
+            return
         self.summary = daily.get('summary')
         self.icon = daily.get('icon')
         self.days = []
@@ -119,12 +203,58 @@ class Daily():
         for day in data:
             self.days.append(_dataPoint(day))
 
+    def printData(self, full=False):
+        print
+        print self.summary
+        if full:
+            for day in self.days:
+                for key in day:
+                    print key, ':', day[key]
+                print
+        else:
+            for day in self.days:
+                print
+                print str(day.time).split(' ')[0]
+                print '\t', day.summary
+                print
+                print '\tTemperature Low: ', day.temperatureMin, 'F'
+                print '\tTemperature High: ', day.temperatureMax, 'F'
+                if day.windSpeed != None:
+                    print '\tWind speed:', day.windSpeed, 'MPH'
+                    print '\tWind bearing (from):', day.windBearing, 'deg'
+                print
+                print '\tPrecipitation chance:', day.precipProbability * 100, '%'
+                if day.precipProbability != 0:
+                    print '\tType:', day.precipType
+                    print '\tIntensity', day.precipIntensity, 'in/hr'
+                if day.precipAccumulation != None:
+                    print '\tAccumulation:', day.precipAccumulation, 'in'
+                print
+                if day.nearestStormDistance != None:
+                    print 'Nearest storm distance:', day.nearestStormDistance, 'mi'
+                    print 'Nearest storm bearing:', day.nearestStormBearing, 'deg'
+                    print
+                print '\tSunrise:', str(day.sunriseTime).split(' ')[1]
+                print '\tSunset:', str(day.sunsetTime).split(' ')[1]
+                print '\tMoon phase:', day.moonPhase * 100, '% full'
+                print
+                print '\tHumidity:', day.humidity * 100, '%'
+                print '\tDew point:', day.dewPoint, 'F'
+                print '\tVisibility:', day.visibility, 'mi'
+                print '\tCloud cover:', day.cloudCover * 100, '%'
+                print '\tPressure:', day.pressure, 'mb'
+                print '\tOzone:', day.ozone
+
 class Location():
     def __init__(self, jsonDict):
         self.latitude = jsonDict.get('latitude')
         self.longitude = jsonDict.get('longitude')
         self.timezone = jsonDict.get('timezone')
         self.offset = jsonDict.get('offset')
+
+    def printData(self):
+        print 'Lat/Long:', self.latitude, self.longitude
+        print 'Timezone:', self.timezone, '/', self.offset, 'offset'
 
 class Weather():
     def __init__(self, jsonDict):
